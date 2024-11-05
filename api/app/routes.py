23c3b0cffa30bc379cbd63876fbabbceac779dac
blamazales <moraeslamazales@gmail.com>
2024-11-04 22:25:32 -0300

    alteracao arquivo vercel

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,6 +1,11 @@
 from app import app
 from flask import render_template
+import os
 
+if __name__=='main':
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
+    
 @app.route('/')
 @app.route('/index')
 def index():
