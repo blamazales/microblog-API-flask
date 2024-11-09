95c0a17e5ba9ad7aec1e784985033a7f06f18610
blamazales <moraeslamazales@gmail.com>
2024-11-09 17:03:37 -0300

    criacao de paginas html

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from flask import request
 import os
 
 if __name__=='main':
@@ -7,12 +8,22 @@ if __name__=='main':
     app.run(host='0.0.0.0', port = port)
     
 @app.route('/')
-@app.route('/index')
-def index():
-    nome = "Bárbara"
-    dados = {"profissao": "Técnica", "Idade":29}
+@app.route('/index', defaults={"nome":"usuário"})
+@app.route('/index/<nome>/<profissao>/<idade>')
+def index(nome, profissao, idade):
+    dados = {"profissao": profissao, "Idade": idade}
     return render_template('index.html', nome=nome, dados=dados)
 
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['GET'])
+def autenticar():
+    usuario = request.args.get('usuario')
+    senha = request.args.get('senha')
+    return "usuario: {usuario} e senha: {senha}".format(usuario,senha)
